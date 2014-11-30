6059044e753efa2dde7459fba10cdd1bb6911fff
thecrazyfox <flo69001@hotmail.fr>
2014-11-30 16:20:44 +0100

    Arian done Patched Ipad responsive bugs

--- javascript/app.js
+++ javascript/app.js
@@ -7,7 +7,36 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         anchors: ['home', 'about', 'skills', 'achievements', 'end'],
-        menu: 'MyMenu'
+        menu: 'MyMenu',
+        afterLoad: function(anchorLink){
+            var $tip1 = $('.tip-arian-1'),
+            $tip2 = $('.tip-arian-2'),
+            $tip3 = $('.tip-arian-3'),
+            $tip4 = $('.tip-arian-4'),
+            $tip5 = $('.tip-arian-5'),
+            $allTips = $('.tip-arian-accueil');
+
+            if(anchorLink == 'home'){
+                $allTips.removeClass('active-tip');
+                $tip1.addClass('active-tip');
+            }
+            else if(anchorLink == 'about'){
+                $allTips.removeClass('active-tip');
+                $tip2.addClass('active-tip');
+            }
+            else if(anchorLink == 'skills'){
+                $allTips.removeClass('active-tip');
+                $tip3.addClass('active-tip');
+            }
+            else if(anchorLink == 'achievements'){
+                $allTips.removeClass('active-tip');
+                $tip4.addClass('active-tip');
+            }
+            else if(anchorLink == 'end'){
+                $allTips.removeClass('active-tip');
+                $tip5.addClass('active-tip');
+            }
+        }
     });
 
 
@@ -19,6 +48,8 @@ $(document).ready(function() {
                 $width = window.innerWidth;
             var $canvasFull,
                 $responsiveRatio;
+            var $colorFullCircle = '#ebebeb',
+                $colorProgressCircle = '#898989';
 
             if($height <= 768 && $width <= 1024){
                 $canvasFull = $('<canvas width="125px" height="125px" />');
@@ -43,7 +74,7 @@ $(document).ready(function() {
             ctx.beginPath();
             ctx.arc(100/$responsiveRatio,100/$responsiveRatio,80/$responsiveRatio,0,2*Math.PI);
             ctx.lineWidth = 20/$responsiveRatio;
-            ctx.strokeStyle = '#ebebeb';
+            ctx.strokeStyle = $colorFullCircle;
             ctx.stroke();
 
 
@@ -52,14 +83,12 @@ $(document).ready(function() {
             ctx.beginPath();
             ctx.arc(100/$responsiveRatio,100/$responsiveRatio,80/$responsiveRatio,-$progress*Math.PI,-$progress_final*Math.PI);
             ctx.lineWidth = 20/$responsiveRatio;
-            ctx.strokeStyle = '#898989';
+            ctx.strokeStyle = $colorProgressCircle;
             ctx.stroke();
 
         });
 
     })(jQuery);
-
-
 });
 
 
