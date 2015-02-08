d2d5e195d7dcc53f60ba63764d042e05e6af2337
thecrazyfox <flo69001@hotmail.fr>
2015-02-09 00:15:49 +0100

    Added Ajax sending form

---
+++ javascript/app2.js
@@ -0,0 +1,167 @@
+// JavaScript Document
+
+(function(){
+	var check = {};
+	
+	check['user_name'] = function(id) {
+		var user_name = document.getElementById(id);				
+		
+		if (user_name.value.length >= 2 && user_name.value) {
+			user_name.className = "form-input correct";
+			return true;
+		} else {
+			user_name.className = "form-input incorrect";
+			return false
+		}
+	};
+	
+	check['user_mail'] = check['user_name'];
+	
+	check['user_subject'] = check['user_name'];
+	
+	check['user_mess'] = check['user_name'];
+	
+	(function() {
+		
+		function addEvent(element, event, funct, bool) {
+			if (element.addEventListener) {
+				element.addEventListener(event, funct, bool);
+			} else {
+				element.attachEvent('on' + event, funct);
+			}
+		}
+		
+		var myForm = document.getElementById('contact_form'),
+			inputs = document.getElementsByClassName('form-input'),
+			inputsLength = inputs.length;
+			
+			for (var i = 0 ; i < inputsLength ; i++) {
+				if (inputs[i].type == 'text' || inputs[i].type == 'textarea') {
+					
+					inputs[i].onkeyup = function() {
+						check[this.id](this.id);
+					};
+				}
+			}
+			
+			addEvent(myForm, 'submit', function(e) {
+				e.preventDefault();
+				
+				var formInputs = document.getElementsByClassName('form-input'),
+					formInputsLen = formInputs.length;
+				
+				for (var i = 0 ; i < formInputsLen ; i++) {
+					
+					formInputs[i].value = formInputs[i].value.trim();
+				}
+				
+			}, true);
+			
+			addEvent(myForm, 'submit', function(e) {
+				
+				e.preventDefault();
+				
+				var result = true;
+				
+				for (var i in check) {
+					result = check[i](i) && result;
+				}
+				if (result) {
+					function submitForm() {
+							var overlay = document.getElementById('overlay'),
+							 	aElement = overlay.firstElementChild,
+							 	actual = aElement.firstElementChild;	
+														
+							var value_name = document.getElementById('user_name').value,
+								value_mail = document.getElementById('user_mail').value,
+								value_subject = document.getElementById('user_subject').value,
+								value_message = document.getElementById('user_mess').value;
+								
+							var xhr = new XMLHttpRequest();
+							
+							
+							xhr.open('POST', 'mail.php');
+							
+							xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+							
+							xhr.onreadystatechange = function() {
+								if (xhr.readyState == 4) {
+
+									var response = xhr.responseText;
+									
+									resp = JSON.parse(response);								
+									
+									if (resp.error == 'true') {
+										
+										var errorMess = document.createTextNode('Erreur dans l\'envoi du message !');
+										actual.style.color = "red";																			
+										actual.replaceChild(errorMess, actual.firstChild);																		
+									}
+									
+									overlay.style.display = "block";
+									actual.lastElementChild.onclick = function() {											
+										overlay.style.display = "none";
+										myForm.reset();
+										return false;
+									};																		
+								}
+							};
+							
+							xhr.send('param1=' + value_name + '&param2=' + value_mail + '&param3=' + value_subject + '&param4=' + value_message);
+						}
+						setTimeout(function(){
+							submitForm()
+						}, 500);
+				}
+
+			}, false);
+			
+			myForm.onreset = function() {
+				
+				for (var i = 0 ; i < inputsLength ; i++) {
+					if (inputs[i].type == 'text' || inputs[i].type == 'textarea') {
+						inputs[i].className = "form-input";
+					}
+				}
+			};
+		
+	})();
+})();
+/*
+(function(){		
+	
+	function submitForm() {
+		var value_name = document.getElementById('user_name').value,
+			value_mail = document.getElementById('user_mail').value,
+			value_subject = document.getElementById('user_subject').value,
+			value_message = document.getElementById('user_mess').value;
+			
+		var xhr = new XMLHttpRequest();
+		
+		
+		xhr.open('POST', 'mail.php');
+		
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4) {
+				var message = 'email envoyé';
+				var messageError = 'email PAS envoyé';
+				alert('no error detected');
+				document.getElementById('test').innerHTML = '<span>' + message + '</span>';
+			}
+			else {
+				alert('erreur !!!!');
+			}
+		};
+		
+		xhr.send('param1=' + value_name + '&param2=' + value_mail + '&param3=' + value_subject + '&param4=' + value_message);
+	}
+	
+	var myForm = document.getElementById('contact_form');
+	
+	myForm.onsubmit = function() {
+		submitForm();
+		return false;
+	};
+})();*/
